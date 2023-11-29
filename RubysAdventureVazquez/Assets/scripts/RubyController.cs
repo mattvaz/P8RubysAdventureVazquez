d702580ac03dd48fa02091a67506fd8f94f8ca84
mattvaz <146034846+mattvaz@users.noreply.github.com>
2023-11-29 15:26:43 -0800

    added many things.
    I added metal cubs more decorations and added new script.

--- RubysAdventureVazquez/Assets/scripts/RubyController.cs
+++ RubysAdventureVazquez/Assets/scripts/RubyController.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+        float vertical;
+
+
+    // Start is alled before the first frame update
     void Start()
     {
-         
+        rigidbody2d = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
     }
+     void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
 }
